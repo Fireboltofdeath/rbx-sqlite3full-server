74b54370b71e5698edfc93bc184a2bd74a50345e
Fireboltofdeath <huntermail213@gmail.com>
2019-01-30 21:01:41 -0500

    Update multiServer.js

--- nodejs/multiServer.js
+++ nodejs/multiServer.js
@@ -1,3 +1,7 @@
+var push = []; // queue functions to load after express loads (ignore)
+// CONFIG //
+const PORT = process.env.PORT; // I recommend against port 80, if you use your own VPS.
+
 // The below variables will be used if you do not specify them for a database //
 var default_ApiToken = ""; // Can be used to set all databases to the same ApiToken
 var default_StartupQueries = [];
@@ -9,22 +13,19 @@ registerDatabase("/database1", "./.view/database1.db", null, ["CREATE TABLE IF N
 registerDatabase("/database2", "./.view/database2.db", "newApiToken"); // specify api token, and use default startup queries
 registerDatabase("/database3", "./.view/database3.db"); // use both default api token and default startupQueries
 */
-registerDatabase("/database1", "./database1_test.db", "ApiToken1", [
+registerDatabase("database1", "./database1_test.db", "ApiToken1", [ // path is https://server.tld/database1/
     "CREATE TABLE IF NOT EXISTS `users` (userid INT, username VARCHAR(80), description VARCHAR(500))"
  ]);
-
-
-const expressMod = require("./expressModule.js");
-
+// CONFIG \\
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+const expressMod = require("./expressModule.js");
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.all("/", function(req, res) {
   res.send("Well, hello there Wanderer!");
 });
-
 
 function registerDatabase(Path, databaseFile, ApiToken, StartupQueries) {
   if (databaseFile == null) { console.error("Database File not specified."); }
@@ -33,14 +34,15 @@ function registerDatabase(Path, databaseFile, ApiToken, StartupQueries) {
   if (ApiToken == null) { ApiToken = default_ApiToken };
   if (StartupQueries == null) { ApiToken = default_StartupQueries };
   if (Path.substring(0, 1) != "/") { Path = "/" + Path };
-  
-  app.use(Path, expressMod({
-    databaseFile: databaseFile,
-    ApiToken: ApiToken,
-    startupQueries: StartpQueries
-  }));
+  push[push.length] = function() {
+    app.use(Path, expressMod({
+      databaseFile: databaseFile,
+      ApiToken: ApiToken,
+      startupQueries: StartupQueries
+    }))
+  }
 }
-
+push.forEach(function(v) { v(); });
 
 var list = app.listen(PORT, function() {
   console.log('Server Online, Port ' + list.address().port);
